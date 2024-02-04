0d5cbc2ad6e1443bc7f0cc35fcae034e20fee837
Aleksi <79052109+Haxeli@users.noreply.github.com>
2024-02-04 11:14:04 +0200

    Enemies will spawn at spawn point position

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class WaveSpawner : MonoBehaviour
 {
     public Transform enemyPrefab;
+    public Transform spawnPoint;
 
     public float timeBetweenWaves = 5f;
     private float countdown = 2f;
+
+    private int waveNumber = 1;
     private void Update()
     {
         if (countdown <= 0f)
@@ -21,6 +24,15 @@ public class WaveSpawner : MonoBehaviour
 
     void SpawnWave()
     {
-        Debug.Log("Wave Incoming");
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+        }
+        waveNumber++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
     }
 }
