66cc4d2b8206c1e1491a7d717c61d22bbd85bff0
Aleksi <79052109+Haxeli@users.noreply.github.com>
2024-02-05 15:45:54 +0200

    Display turret range with Gizmos

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public Transform target;
+    public float range = 15f;
+
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
