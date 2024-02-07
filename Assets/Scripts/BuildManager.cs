731356d485aeba26071ece84a4c734fa9a174577
Aleksi <79052109+Haxeli@users.noreply.github.com>
2024-02-07 18:30:48 +0200

    Update BuildManager.cs

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class BuildManager : MonoBehaviour
 {
+    public static BuildManager instance;
     private GameObject turretToBuild;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
 }
