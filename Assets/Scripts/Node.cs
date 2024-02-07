11335aa9f3a4795deed3ca23d71159e4702d804f
Aleksi <79052109+Haxeli@users.noreply.github.com>
2024-02-07 18:20:50 +0200

    Added OnMouseDown function

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -5,11 +5,20 @@ public class Node : MonoBehaviour
     public Color hoverColor;
     private Renderer rend;
     private Color startColor;
+    private GameObject turret;
 
     private void Start()
     {
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Can't build there");
+        }
     }
 
     private void OnMouseEnter()
