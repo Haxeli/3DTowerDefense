14512d608f218b7365f8b485b2bc43302bc9d94a
Aleksi <79052109+Haxeli@users.noreply.github.com>
2024-02-07 18:07:15 +0200

    Node changes color upon mouse enter and exit

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Node : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Color hoverColor;
+    private Renderer rend;
+    private Color startColor;
+
+    private void Start()
     {
-        
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseEnter()
     {
-        
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
     }
 }
