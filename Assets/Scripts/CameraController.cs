197814aec91aa8600bc28277897a1a27195cd0fc
Aleksi <79052109+Haxeli@users.noreply.github.com>
2024-02-08 22:24:11 +0200

    Camera moves up according to mouse

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
 
     void Update()
     {
-        if (Input.GetKey("w"))
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
             transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
         }
