69c53e07ce6c00677cb1460ed14990dea803f99f
Aleksi <79052109+Haxeli@users.noreply.github.com>
2024-02-08 22:50:15 +0200

    Added zoom constraints

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,6 +11,8 @@ public class CameraController : MonoBehaviour
     public float panSpeed = 30f;
     public float panBorderThickness = 10f;
     public float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 80f;
 
     void Update()
     {
@@ -37,7 +39,10 @@ public class CameraController : MonoBehaviour
         float scroll = Input.GetAxis("Mouse ScrollWheel");
 
         Vector3 pos = transform.position;
+
         pos.y -= scroll * 3000 * scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
         transform.position = pos;
     }
 }
